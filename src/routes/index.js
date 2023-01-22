61d52bd007916b68c7f5ce607001e2cc84e660f8
marylaurab <mbetancourt@fi.uba.ar>
2023-01-22 13:56:19 -0400

    index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const userMiddleware = require("./user");
+const eventMiddleware = require("./event");
+const loginMiddleware = require("./login");
 
+router.use("/user", userMiddleware);
+router.use("/event", eventMiddleware);
+router.use("/login", loginMiddleware);
 
+module.exports = router;
 
 module.exports = router;
