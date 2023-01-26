4edbbca713e0ad5799b56d918c6a4741f76b6a73
marylaurab <mbetancourt@fi.uba.ar>
2023-01-26 19:48:24 -0400

    modif de models (category y event). Rutas de filtraje (falta eventsToday por incorporar)

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,7 +3,10 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('category',{
-        name: {
+        modalityName:{
+            type: DataTypes.ENUM('PRESENTIAL', 'VIRTUAL')
+        },
+        categoryName: { //subcategorias
             type: DataTypes.STRING(20),
             allowNull: true,
         },              
