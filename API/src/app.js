fa42be9db1dbae25e0ff323d81c911d0ae1892e3
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-01-30 13:27:27 -0300

    documentation finished!

--- API/src/app.js
+++ API/src/app.js
@@ -7,8 +7,8 @@ const cookieParser = require('cookie-parser')
 const routes = require('./routes/index');
 const YAML = require('yamljs');
 
-
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
@@ -19,8 +19,14 @@ app.use(bodyParser.json({ limit: '50mb' }));
 const swaggerDocument = YAML.load('./src/docs/doc.yaml');
 const swaggerUi = require('swagger-ui-express');
 
+var options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Documentacion API",
+  customfavIcon: "/assets/favicon.ico"
+};
+
 app.use('/', routes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 // Error catching endware.
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
