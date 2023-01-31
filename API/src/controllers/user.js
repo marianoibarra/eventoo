432b0d82cfa32a43588a435b6f1956889b2c58b7
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-01-31 14:04:14 -0300

    fix(api/endMail): Config send mailer

--- API/src/controllers/user.js
+++ API/src/controllers/user.js
@@ -62,7 +62,7 @@ const register = async (req, res) => {
       }
     );
 
-    // sendEmail(newUser.email, code, newUser.name, "confirmEmail");
+    sendEmail(newUser.email, code, newUser.name, "confirmEmail");
 
     const token = jwt.sign({ id: newUser.id }, process.env.SECRET, {
       expiresIn: "90d",
