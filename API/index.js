59c834d0296dc41d7214ce386effcbf9bdb3f719
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-01 00:08:12 -0300

    set(api/index) sync force false

--- API/index.js
+++ API/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const fillTables = require('./src/helpers/fillTables.js');
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true })
+conn.sync({ force: false })
   .then(() => fillTables())
   .then(() =>
     app.listen(port, () => console.log(`Listening on port ${port}!`))
