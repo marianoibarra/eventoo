e2596ac4f6f5c0c4a3d8b74f018ab97c925a0093
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-02 12:31:59 -0300

    feature(api/index.js): error handler

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 ;const app = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const fillTables = require('./src/helpers/fillTables.js');
+require("./src/helpers/error-handler");
 const port = process.env.PORT || 3001;
 
 conn.sync({ force: true })
