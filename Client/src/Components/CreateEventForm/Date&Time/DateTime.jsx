aeadd99ba644b277d6e7c187b7b5dd22f3c82eb7
paolagll <pao__1105@hotmail.com>
2023-02-02 01:27:11 -0300

    dateandtimeinput

--- Client/src/Components/CreateEventForm/Date&Time/DateTime.jsx
+++ Client/src/Components/CreateEventForm/Date&Time/DateTime.jsx
@@ -5,11 +5,14 @@ function DateTime(){
         <div className={style.info}>
             <h2 className={style.title}>Date and Time</h2>
             <p className={style.text}>Please choose the date and time that your event will take place. It's important to select the correct hour to ensure that your guests arrive on time.</p>
-            <input></input>
+            <h4 className={style.title}>Start Date</h4>
+            <input type='date' format='aaaa-mm-dd'></input>
+            <h4 className={style.title}>End date</h4>
+            <input type='date' format='aaaa-mm-dd'></input>
             <h4 className={style.title}>Start time</h4>
-            <input></input>
+            <input type='time'></input>
             <h4 className={style.title}>End time</h4>
-            <input></input>
+            <input type='time'></input>
         </div>
     )
 };
