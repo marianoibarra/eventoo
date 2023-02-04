eda08feb60f0f17356ac7df3a738096e2bc39952
fede-cid <fede.cid.96@gmail.com>
2023-02-04 02:14:44 -0300

    login/create/REDUCER

--- Client/src/Store/index.js
+++ Client/src/Store/index.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { categorieSlice } from "../Slice/Filter/categorieSlice";
+import { authSlice } from "../Slice/LoginForm/LoginFormSlice";
+import { registerSlice } from "../Slice/CreateUse/CreateUserSlice";
+import { UserSlice } from "../Slice/User/UserSlice"
+import { eventsSlice } from "../Slice/Events/EventsSlice";
+import { eventSlice } from "../Slice/CreateEvent/CreateEvent";
 
 const store = configureStore({
   reducer: {
-    categories: categorieSlice.reducer
+    categories: categorieSlice.reducer,
+    auth: authSlice.reducer,
+    register:registerSlice.reducer,
+    user:UserSlice.reducer,
+    events: eventsSlice.reducer,
+    event: eventSlice.reducer,
   },
 });
 
