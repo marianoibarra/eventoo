7cc9cc6719763b279a4a649943d8cbb70b34064f
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-03 23:16:08 -0300

    fix error

--- API/index.js
+++ API/index.js
@@ -2,7 +2,6 @@ const app = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config();
 const fillTables = require('./src/helpers/fillTables.js');
-require("./src/helpers/error-handler");
 const port = process.env.PORT || 3001;
 
 process.on('uncaughtException', (err) => {
