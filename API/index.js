ee24b952c5cadf4d345cb82464e41f4fa75254ad
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-03 23:11:30 -0300

    modify db

--- API/index.js
+++ API/index.js
@@ -10,7 +10,7 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-conn.sync({ force: false })
+conn.sync({ force: true })
   .then(() => fillTables())
   .then(() =>
     app.listen(port, () => console.log(`Listening on port ${port}!`))
