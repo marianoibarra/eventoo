d89b5542b09dcadcd8c956da22a0c71edc06e828
Mariano Ibarra <74881239+MarianoIT1@users.noreply.github.com>
2023-02-03 21:04:51 -0300

    Update category.js

--- API/src/mocks/category.js
+++ API/src/mocks/category.js
@@ -1,5 +1,5 @@
 const categoryMock = [
-  { name: "Conciertossss", modality: 'Presential'},
+  { name: "Conciertoss", modality: 'Presential'},
   { name: "Fairs", modality: 'Presential'},
   { name: "Festivals", modality: 'Presential'},
   { name: "Business meetings", modality: 'Presential'},
