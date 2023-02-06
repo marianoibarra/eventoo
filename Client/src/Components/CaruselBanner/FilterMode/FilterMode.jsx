fd75380cbf9da9654a918fe54f5b3c94e0de185b
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2023-02-06 18:43:25 -0300

    createHome_endPoint Quinta

--- Client/src/Components/CaruselBanner/FilterMode/FilterMode.jsx
+++ Client/src/Components/CaruselBanner/FilterMode/FilterMode.jsx
@@ -4,48 +4,28 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { axiosModeCategories } from "../../../Slice/Filter/categorieSlice";
 import { axiosCombinedFilter } from '../../../Slice/Filter/combinedFilterSlice';
+import { axiosModeEvents } from '../../../Slice/Events/EventsSlice'
 import Style from './FilterMode.module.css'
 
 const FilterMode = () => {
   const dispatch = useDispatch();
   const { categories, loading } = useSelector(state => state.categories.categories);
+  const { events } = useSelector(state => state.events)
+
+
   const [selectedModality, setSelectedModality] = useState(null);
   const [filteredCategories, setFilteredCategories] = useState([]);
 
+  const [value, setValue] = useState('')
+  const [filteredSuggestions, setFilteredSuggestions] = useState([])
+
+
+  //BLOQUE DE CODIGO PARA SUPERQUERY
   //Const para el filtro SuperQuery
   const [city, setCity] = useState(null)
   const [category, setCategory] = useState(null)
-
-  const filter = {
-    modality: selectedModality,
-    city: city,
-    category: category
-  }
-  let query = {}
-  let resultSuperQuery = ''
-
-  for (let prop in filter) {
-    if (filter[prop] !== null) {
-      query[prop] = filter[prop]
-    }
-  }
-  for (let prop in query) {
-    if (query[prop] === null || query[prop].length > 1)
-    {  resultSuperQuery += `&${prop}=${query[prop]}`}
-    dispatch( axiosCombinedFilter(resultSuperQuery));
-  }
-
-  useEffect(() => {
-    dispatch(axiosModeCategories());
-    dispatch( axiosCombinedFilter(resultSuperQuery))
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (!loading && categories) {
-      setFilteredCategories(categories.filter(c => c.modality === selectedModality));
-    }
-  }, [categories, loading, selectedModality]);
-
+  const [weekend, setWeekend] = useState(null)
+  const [today, setToday] = useState(null)
 
 
   const handleChange = e => {
@@ -58,6 +38,61 @@ const FilterMode = () => {
 
   const handleCategoryChange = e => {
     setCategory(e.target.value)
+  }
+
+  const handleWeekendChange = e => {
+    setWeekend(e.target.value)
+  }
+
+  const handleTodayChange = e => {
+    setToday(e.target.value)
+  }
+
+  const filter = {
+    modality: selectedModality,
+    city: city,
+    category: category,
+    isToday: today,
+    isNextWeekend: weekend
+  }
+  let query = {}
+  let resultSuperQuery = ''
+  //For in que filtra los null del filter
+  for (let prop in filter) {
+    if (filter[prop] !== null) {
+      query[prop] = filter[prop]
+    }
+  }
+
+  //For in que concatena el query para armar la URL
+  for (let prop in query) {
+    if (query[prop] === null || query[prop].length > 1) { resultSuperQuery += `&${prop}=${query[prop]}` }
+    dispatch(axiosCombinedFilter(resultSuperQuery));
+  }
+
+
+  useEffect(() => {
+    dispatch(axiosModeCategories());
+    dispatch(axiosModeEvents())
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!loading && categories) {
+      setFilteredCategories(categories.filter(c => c.modality === selectedModality));
+    }
+  }, [categories, loading, selectedModality]);
+
+
+
+
+
+
+  const handleNameEvent = (e) => {
+    const value = e.target.value
+    console.log(value, 'desde el input')
+    setValue(value)
+    setFilteredSuggestions(events.filter(e => e.name.toLowerCase().startsWith(value.toLowerCase())))
+
   }
 
   return (
@@ -84,11 +119,22 @@ const FilterMode = () => {
         <input placeholder='' onChange={handleCityChange} ></input>
       </div>
 
+
       <div className={Style.search}>
         <h2 className={Style.titleFilter}>Search</h2>
-        <select className={Style.select_Looking} name="" id="">
-          <option selected disabled hidden>event</option>
-        </select>
+        <input placeholder='' onChange={handleNameEvent} />
+        <ul>
+          {filteredSuggestions.map(s => {
+            <li key={s}>{s}</li>
+          })}
+        </ul>
+      </div>
+
+      <div className={Style.select_date}>
+        <label>
+          <input type="checkbox" checked={weekend} onChange={handleWeekendChange} /> Weekend </label>
+        <label>
+          <input type="checkbox" checked={today} onChange={handleTodayChange} />Today </label>
       </div>
 
     </div>
