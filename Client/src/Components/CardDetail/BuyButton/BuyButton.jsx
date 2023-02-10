42dab07ca3bcc28d9c6f8d5f0914a61c93eef4bb
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2023-02-10 20:13:28 -0300

    ModalTransaction

--- Client/src/Components/CardDetail/BuyButton/BuyButton.jsx
+++ Client/src/Components/CardDetail/BuyButton/BuyButton.jsx
@@ -1,43 +1,50 @@
 import React, { useState } from "react";
 import style from "./BuyButton.module.css";
+import ModalTransaction from '../../Modal/ModalTransaction/ModalTransaction'
+
+
 
 const BuyButton = () => {
   const price = 150;
   const [tickets, setTickets] = useState(1);
   const totalPrice = price * tickets;
+  const [showModal, setShowModal] = useState(false)
 
-  function handleButtonSubtraction(){
+
+  function handleButtonSubtraction() {
     setTickets(tickets - 1);
   }
 
-  function handleButtonAddition(){
+  function handleButtonAddition() {
     setTickets(tickets + 1);
   }
 
-  function handleOnClick(event){
+  function handleOnClick(event) {
     event.preventDefault();
   }
 
   return (
+    
     <div className={style.containerbottomright}>
       <div className={style.buycontainer}>
         <div className={style.container_text_and_tickets}>
-            <div className={style.divtext}>
-                <p>
-                    <b>Advance Tickets</b>
-                </p>
-                <p className={style.price}>{"$" + price}</p>
-            </div>
-            <div className={style.containertickets}>
-                <button onClick={handleButtonSubtraction} disabled={tickets === 1 ? true : false}><span>−</span></button>
-                <span><b>{tickets}</b></span>
-                <button onClick={handleButtonAddition} disabled={tickets === 10 ? true : false}><span>+</span></button>
-            </div>
+          <div className={style.divtext}>
+            <p>
+              <b>Advance Tickets</b>
+            </p>
+            <p className={style.price}>{"$" + price}</p>
+          </div>
+          <div className={style.containertickets}>
+            <button onClick={handleButtonSubtraction} disabled={tickets === 1 ? true : false}><span>−</span></button>
+            <span><b>{tickets}</b></span>
+            <button onClick={handleButtonAddition} disabled={tickets === 10 ? true : false}><span>+</span></button>
+          </div>
         </div>
         <div>
-          <a className={`btnprimario ${style.buybutton}`} href="" onClick={handleOnClick}>
-            <span>{`Buy by $${totalPrice}`}</span>
-          </a>
+          <button className={`btnprimario ${style.buybutton}`} href="" onClick={()=>setShowModal(!showModal)}>{`Buy by $${totalPrice}`}</button>
+          {showModal && <ModalTransaction 
+          setShowModal={setShowModal}
+          tickets={tickets}/> }
         </div>
       </div>
     </div>
---
+++ Client/src/Components/Tickets/Tickets.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './Tickets.module.css'
+
+const Tickets = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tickets
