463135b6ac94b75a5a38b791cdba8fa9ddc4f72a
marylaurab <mbetancourt@fi.uba.ar>
2023-02-11 23:31:34 -0300

    propiedad columna modelo category

--- API/src/models/Category.js
+++ API/src/models/Category.js
@@ -1,19 +1,22 @@
-const { DataTypes } = require('sequelize');
-
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('category',{
-        name: { 
-            type: DataTypes.STRING(40),
-            allowNull: true,
-        },   
-        modality:{
-            type: DataTypes.ENUM('Presential', 'Virtual')
-        },
-        isActive:{
-            type: DataTypes.BOOLEAN
-        }
-    }, {
-        timestamps: false
-    })
+  sequelize.define(
+    "category",
+    {
+      name: {
+        type: DataTypes.STRING(40),
+        allowNull: true,
+      },
+      modality: {
+        type: DataTypes.ENUM("Presential", "Virtual"),
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
