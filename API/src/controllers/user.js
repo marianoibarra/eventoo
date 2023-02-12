b00f2196b2f542fc2dd9b5e1049f243d62c0a627
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-12 12:26:47 -0300

    changes on response of user

--- API/src/controllers/user.js
+++ API/src/controllers/user.js
@@ -98,6 +98,20 @@ const register = async (req, res) => {
       }
     );
 
+    const response = await User.findByPk(newUser.id, {
+      attributes: { exclude: ["password", "addressId", "roleAdminId"] },
+      include: [
+        "roleAdmin",
+        {
+          model: Address,
+          as: "address",
+          attributes: { exclude: ["id"] },
+        },
+      ],
+    }).then((r) => r.toJSON());
+
+    response.roleAdmin = response.roleAdmin.name;
+
     sendEmail(newUser.email, code, newUser.name, "confirmEmail");
 
     const token = jwt.sign({ id: newUser.id }, process.env.SECRET, {
@@ -108,12 +122,7 @@ const register = async (req, res) => {
       msg: "User created successfully",
       id: newUser.id,
       token,
-      data: {
-        name,
-        last_name,
-        email,
-        profile_pic,
-      },
+      data: response,
     });
   } catch (e) {
     console.log(e);
@@ -196,8 +205,17 @@ const login = async (req, res) => {
 
     const user = await User.findOne({
       where: { email },
-      include: "address",
+      attributes: { exclude: ["addressId", "roleAdminId"] },
+      include: [
+        "roleAdmin",
+        {
+          model: Address,
+          as: "address",
+          attributes: { exclude: ["id"] },
+        },
+      ],
     });
+
     if (!user) {
       return res.status(401).send({ msg: "Email or password is incorrect" });
     }
@@ -211,18 +229,19 @@ const login = async (req, res) => {
       expiresIn: "90d",
     });
 
+     const response = await user.toJSON()  
+    
+     response.roleAdmin = response.roleAdmin.name;
+     delete response.password
+
     return res.send({
       msg: "Logged in successfully",
       id: user.id,
       token,
-      data: {
-        name: user.name,
-        last_name: user.last_name,
-        email: user.email,
-        profile_pic: user.profile_pic,
-      },
+      data: response,
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).send({ msg: "Internal server error" });
   }
 };
@@ -300,23 +319,26 @@ const checkResetToken = async (req, res) => {
   }
 };
 
-const getProfile = async ({userId},res) => {
- 
-try {
-
-  const profileUser = await User.findByPk(userId, {
-    attributes: { exclude: ["password"] },
-    include: [{
-      model: Address,
+const getProfile = async ({ userId }, res) => {
+  console.log(userId);
+  try {
+    const profileUser = await User.findByPk(userId, {
+      attributes: { exclude: ["password", "addressId", "roleAdminId"] },
+      include: [
+        "roleAdmin",
+        {
+          model: Address,
           as: "address",
-          attributes: { exclude: ["id"] }
-  }]
-  });
-  res.status(200).json(profileUser);
-  
-} catch (error) {
-  res.status(500).json({ msg: error.message });
-}
+          attributes: { exclude: ["id"] },
+        },
+      ],
+    }).then((r) => r.toJSON());
+
+    profileUser.roleAdmin = profileUser.roleAdmin.name;
+    res.status(200).json(profileUser);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 
 const resetPassword = async (req, res) => {
@@ -410,7 +432,7 @@ const verifyAdmins = async (req, res, next) => {
     if (role.name === "ADMIN" || role.name === "SUPERADMIN") {
       next();
     } else {
-      res.status(401).json({ msg: "You are not an ADMIN" }); 
+      res.status(401).json({ msg: "You are not an ADMIN" });
     }
   } catch (error) {
     res.status(404).json({ error: error.message });
@@ -434,7 +456,7 @@ const verifySuperAdmin = async (req, res, next) => {
 
 const changeRole = async (req, res) => {
   const { id } = req.params;
-  const userId= Number(id)
+  const userId = Number(id);
   try {
     const user = await User.findByPk(userId);
     const roleId = user.roleAdminId;
