f5dec1612637571e1906057b54f466221dc37d71
marylaurab <mbetancourt@fi.uba.ar>
2023-02-12 00:12:44 -0300

    get de users testeado y listo

--- API/src/controllers/admin.js
+++ API/src/controllers/admin.js
@@ -1,4 +1,24 @@
-const { Category } = require("../db");
+const { Category, User, Address, RoleAdmin } = require("../db");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      include: [
+        {
+          model: Address,
+          as: "address",
+          attributes: { exclude: ["id"] },
+        },
+        {
+          model: RoleAdmin,
+        },
+      ],
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
 
 const getCategories = async (req, res) => {
   const { id } = req.params;
@@ -18,4 +38,5 @@ const getCategories = async (req, res) => {
 
 module.exports = {
   getCategories,
+  getUsers,
 };
--- API/src/routes/admin.js
+++ API/src/routes/admin.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getCategories } = require("../controllers/admin");
+const { getUsers,getCategories } = require("../controllers/admin");
 const { verifyToken, verifyAdmins } = require("../controllers/user");
 
 const router = Router();
 
+router.get('/users', verifyToken, verifyAdmins, getUsers)
 router.put("/categories/:id", verifyToken, verifyAdmins, getCategories);
 
 module.exports = router;
