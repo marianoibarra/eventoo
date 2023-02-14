bedeedf2bfeb8bd3e1b06d75567e4c2899298f3f
paolagll <pao__1105@hotmail.com>
2023-02-14 17:00:17 -0300

    fix preview img & input hr

--- Client/src/Components/CreateEventForm/CreateEventForm.jsx
+++ Client/src/Components/CreateEventForm/CreateEventForm.jsx
@@ -14,15 +14,15 @@ import ModalBank from '../Modal/ModalBank/ModalBank';
 import { getBankAccounts } from '../../Slice/BankAcount/BankAcount';
 import ModalFormEvent from '../Modal/ModalFormEvent/ModalFormEvent';
 import { SessionContext } from '../..';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
-function Form(){
+function Form() {
   const dispatch = useDispatch();
   const event = useSelector(state => state.event);
-  const {isLogged} = useSelector(state => state.user);
+  const { isLogged } = useSelector(state => state.user);
   const [selectedModality, setSelectedModality] = useState('Presential');
-  const[showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const { setShowSessionModal } = useContext(SessionContext);
 
   const stgData = JSON.parse(localStorage.getItem("formEvent"))
@@ -33,47 +33,45 @@ function Form(){
 
 
   const initialState = {
-    address_line:null,
-    adversiting_end:null,
-    advertisingTime_start:null,
-    age_range:'ALL PUBLIC',
-    bankAccount:'',
-    category:null,
-    city:null,
-    country:null,
-    cover_pic:null,
-    description:null,
-    disability_access:null,
-    end_date:'',
-    end_time:'',
-    guests_capacity:"",
+    address_line: null,
+    adversiting_end: null,
+    advertisingTime_start: null,
+    age_range: 'ALL PUBLIC',
+    bankAccount: '',
+    category: null,
+    city: null,
+    country: null,
+    cover_pic: null,
+    description: null,
+    disability_access: null,
+    end_date: '',
+    end_time: '',
+    guests_capacity: "",
     modality: '',
-    isPaid:true,
-    isPremium:null,
-    isPublic:true,
-    name:'',
-    parking:null,
-    pet_friendly:null,
-    placeName:null,
-    price:'',
-    smoking_zone:null,
-    start_date:'',
-    start_time:'',
-    state:null,
-    virtualURL:'',
-    zip_code:null,
+    isPaid: true,
+    isPremium: null,
+    isPublic: true,
+    name: '',
+    parking: null,
+    pet_friendly: null,
+    placeName: null,
+    price: null,
+    smoking_zone: null,
+    start_date: '',
+    start_time: '',
+    state: null,
+    virtualURL: '',
+    zip_code: null,
   }
-  
+
 
   const [input, setInput] = useState(initialState);
   const [errors, setErrors] = useState({});
-  const [showMsg, setShowMsg] = useState ({});
-
-  const navigate = useNavigate()
+  const [showMsg, setShowMsg] = useState({});
 
   function validate(input) {
-    let errors={}
-    if(!input.description) {errors.description = 'Description es required'};
+    let errors = {}
+    if (!input.description) { errors.description = 'Description es required' };
     if (input.name.length === 0) {
       errors.name = "Title is required";
     } else if (input.name.length < 4) {
@@ -81,23 +79,25 @@ function Form(){
     } else if (input.name.length > 40) {
       errors.name = "Title must have a maximum of 40 characters";
     }
-    if(!input.category) {errors.category = 'You must select a category'}
-    
-    if (selectedModality === 'Virtual' && (input.virtualURL === null || input.virtualURL.length === 0)) {errors.virtualURL = "URL is required";
+    if (!input.category) { errors.category = 'You must select a category' }
+
+    if (selectedModality === 'Virtual' && (input.virtualURL === null || input.virtualURL.length === 0)) {
+      errors.virtualURL = "URL is required";
     } else if (selectedModality === 'Virtual' && input.virtualURL.length < 10) {
       errors.virtualURL = "URL must have at least 10 characters";
     }
-    if(!input.address_line && selectedModality === 'Presential') {errors.address_line = 'Address is required'};
+    if (!input.address_line && selectedModality === 'Presential') { errors.address_line = 'Address is required' };
 
     const now = new Date();
     const startDate = new Date(input.start_date);
 
-    if(!input.start_date){errors.start_date = 'Start date is required'}
-    if (startDate < now ){errors.start_date = 'Start date cannot be in the past'}
-    if(!input.end_date){errors.end_date = 'End date is required'
-    } else if (input.end_date < input.start_date) {errors.end_date = 'End date can not be before the start date'}
-    if(!input.start_time){errors.start_time = 'Start time is required'}
-    if(!input.end_time){errors.end_time = 'End date is required'}
+    if (!input.start_date) { errors.start_date = 'Start date is required' }
+    if (startDate < now) { errors.start_date = 'Start date cannot be in the past' }
+    if (!input.end_date) {
+      errors.end_date = 'End date is required'
+    } else if (input.end_date < input.start_date) { errors.end_date = 'End date can not be before the start date' }
+    if (!input.start_time) { errors.start_time = 'Start time is required' }
+    if (!input.end_time) { errors.end_time = 'End date is required' }
     if (input.start_date === input.end_date) {
       const start = new Date(`${input.start_date} ${input.start_time}`);
       const end = new Date(`${input.end_date} ${input.end_time}`);
@@ -106,33 +106,33 @@ function Form(){
       }
     }
     const num = parseInt(input.guests_capacity)
-    if(!input.guests_capacity ){errors.guests_capacity = 'Capacity is required'
-    }else if( isNaN(num) || num < 0) { errors.guests_capacity = 'Enter a valid number'};
+    if (!input.guests_capacity) {
+      errors.guests_capacity = 'Capacity is required'
+    } else if (isNaN(num) || num < 0) { errors.guests_capacity = 'Enter a valid number' };
 
-    if(input.isPaid === true && (input.price === null || input.price.length === 0)){
+    if (input.isPaid === true && (input.price === null || input.price.length === 0)) {
       errors.price = 'Price is required'
-    } else if( (input.isPaid === true) && isNaN (parseInt(input.price)) || (parseInt(input.pice)) < 0){
+    } else if ((input.isPaid === true) && isNaN(parseInt(input.price)) || (parseInt(input.pice)) < 0) {
       errors.price = 'Enter a valid number';
     }
 
     console.log(errors)
     return errors;
   }
-  
+
   const [confirm, setConfirm] = useState(null)
 
   useEffect(() => {
     setErrors(validate(input));
-    if(confirm !== null) {
-      localStorage.setItem("formEvent" , JSON.stringify(input));
-      localStorage.setItem("lastTime" , new Date());
+    console.log(input)
+    if (confirm !== null) {
+      localStorage.setItem("formEvent", JSON.stringify(input));
+      localStorage.setItem("lastTime", new Date());
     }
   }, [input]);
 
-
-
   useEffect(() => {
-    if(stgData && stgData.name !== null & stgData.name.length > 0) {
+    if (stgData && stgData.name !== null & stgData.name.length > 0) {
       setShowModal(true)
     } else {
       setConfirm(false)
@@ -141,9 +141,9 @@ function Form(){
 
 
   useEffect(() => {
-    if(confirm === true) {
+    if (confirm === true) {
       setInput(stgData)
-    } else if(confirm === false) {
+    } else if (confirm === false) {
       localStorage.removeItem("formEvent");
       localStorage.removeItem("lastTime");
     }
@@ -152,7 +152,7 @@ function Form(){
 
   const handleSubmit = e => {
     e.preventDefault();
-    if(isLogged){
+    if (isLogged) {
       dispatch(createEvent(input))
       alert("Event created!")
     } else {
@@ -161,33 +161,40 @@ function Form(){
     };
   };
 
-  return(
+  return (
     <div className={style.container}>
-        { showModal &&  <ModalFormEvent stgData={stgData} setConfirm={setConfirm} setShowModal={setShowModal}/>}
-        {/* <Lateral/> */}
-        
-        <form className={style.form} onSubmit={e=>handleSubmit(e)}>
-          <h1 className={style.title}>EVENT INFORMATION</h1>
-          <UploadImage input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg}/>
-          <div className={style.split}></div>
-          <BasicInfo input = {input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg}/>
-          {/* <div className={style.split}></div>
-          <MoreInfo/> */}
-          <div className={style.split}></div>
-          <h1 className={style.title}>LOCATION AND CATEGORY</h1>
-          <Category input = {input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} selectedModality={selectedModality} setSelectedModality={setSelectedModality} />
-          <div className={style.split}></div>
-          <DateTime input = {input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg}/>
-          <div className={style.split}></div>
-          <Tickets input = {input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg}/>
-          {event.error ? <p className={style.errorMessage}>Can't create event</p> :
-            event.create ? <p className={style.sendMessage}>Event created successfully</p>: undefined}
-          {/* <button type='button' className={style.btnprimario} onClick={() => setShowModal(!showModal)}>Bank Account</button> */}
-          <div className={style.footerForm}>
-            {/* <button type='button' >Save changes</button> */}
-            <button type='submit' className={style.btnprimario} disabled={Object.keys(errors).length !== 0} >Create</button>
+      {showModal && <ModalFormEvent stgData={stgData} setConfirm={setConfirm} setShowModal={setShowModal} />}
+      {/* <Lateral/> */}
+
+      <form className={style.form} onSubmit={e => handleSubmit(e)}>
+        <h1 className={style.title}>EVENT INFORMATION</h1>
+        {input.cover_pic !== null?
+          <div className={style.containerimg}>
+          <img className={style.img} src={input.cover_pic} alt='cover_pic' /> 
           </div>
-        </form>
+         : <div className={style.containerimg}> <h3 className={style.subtitle}>Upload your event image</h3></div>
+        }
+
+        <UploadImage input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} />
+        <div className={style.split}></div>
+        <BasicInfo input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} />
+        {/* <div className={style.split}></div>
+          <MoreInfo/> */}
+        <div className={style.split}></div>
+        <h1 className={style.title}>LOCATION AND CATEGORY</h1>
+        <Category input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} selectedModality={selectedModality} setSelectedModality={setSelectedModality} />
+        <div className={style.split}></div>
+        <DateTime input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} />
+        <div className={style.split}></div>
+        <Tickets input={input} setInput={setInput} errors={errors} showMsg={showMsg} setShowMsg={setShowMsg} />
+        {event.error ? <p className={style.errorMessage}>Can't create event</p> :
+          event.create ? <p className={style.sendMessage}>Event created successfully</p> : undefined}
+        {/* <button type='button' className={style.btnprimario} onClick={() => setShowModal(!showModal)}>Bank Account</button> */}
+        <div className={style.footerForm}>
+          {/* <button type='button' >Save changes</button> */}
+          <button type='submit' className={style.btnprimario} disabled={Object.keys(errors).length !== 0} >Create</button>
+        </div>
+      </form>
     </div>
   )
 };
--- Client/src/Components/CreateEventForm/Date&Time/DateTime.jsx
+++ Client/src/Components/CreateEventForm/Date&Time/DateTime.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateEndDate, updateEndTime, updateStartDate, updateStartTime } from '../../../Slice/CreateEvent/CreateEvent';
 import style from './DateTime.module.css'
 
-function DateTime({input,setInput,errors, showMsg, setShowMsg}){
+function DateTime({ input, setInput, errors, showMsg, setShowMsg }) {
     const dispatch = useDispatch();
 
     const handleChange = (e) => {
@@ -50,7 +50,7 @@ function DateTime({input,setInput,errors, showMsg, setShowMsg}){
     //     })
     // };
 
-    const handleBlur = (e) =>{
+    const handleBlur = (e) => {
         setShowMsg({
             ...showMsg,
             [e.target.name]: true,
@@ -58,58 +58,64 @@ function DateTime({input,setInput,errors, showMsg, setShowMsg}){
     }
 
 
-    return(
+    return (
         <div className={style.info}>
             <h2 className={style.title}>Date and Time</h2>
             <p className={style.text}>
                 Please choose the date and time that your event will take place. It's important to select the correct hour to ensure that your guests arrive on time.
             </p>
-            <h4 className={style.title}>Start</h4>
-            <input
-            type='date'
-            className={style.inputs}
-            value={input.start_date}
-            name = 'start_date'
-            onChange={handleChange}
-            onBlur={handleBlur} 
-            style={ showMsg.start_date && errors.start_date ? {border:'red 1px solid'}: {}}/>
-            {showMsg.start_date&&(
-                            <p className={style.warning}>{errors.start_date}</p>
-                        )}
-            <input 
-            type='time' 
-            className={style.inputs}
-            name='start_time'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={input.start_time}
-            style={ showMsg.start_time && errors.start_time ? {border:'red 1px solid'}: {}}/>
-            {showMsg.start_time&&(
-                            <p className={style.warning}>{errors.start_time}</p>
-                        )}
+            <div>
+                <h4 className={style.title}>Start</h4>
+                <div className={style.datetime}>
+                    <input
+                        type='date'
+                        className={style.inputs}
+                        value={input.start_date}
+                        name='start_date'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={showMsg.start_date && errors.start_date ? { border: 'red 1px solid' } : {}} />
+                    {showMsg.start_date && (
+                        <p className={style.warning}>{errors.start_date}</p>
+                    )}
+                    <input
+                        type='time'
+                        className={style.inputs}
+                        name='start_time'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={input.start_time}
+                        style={showMsg.start_time && errors.start_time ? { border: 'red 1px solid' } : {}} />
+                    {showMsg.start_time && (
+                        <p className={style.warning}>{errors.start_time}</p>
+                    )}
+                </div>
+            </div>
             <h4 className={style.title}>End</h4>
+            <div className={style.datetime}>
+                <input
+                type='date'
+                className={style.inputs}
+                name='end_date'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={input.end_date}
+                style={showMsg.end_date && errors.end_date ? { border: 'red 1px solid' } : {}} />
+            {showMsg.end_date && (
+                <p className={style.warning}>{errors.end_date}</p>
+            )}
             <input
-            type='date'
-            className={style.inputs}
-            name='end_date'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={input.end_date}
-            style={ showMsg.end_date && errors.end_date ? {border:'red 1px solid'}: {}}/>
-            {showMsg.end_date&&(
-                            <p className={style.warning}>{errors.end_date}</p>
-                        )}
-            <input
-            type='time'
-            className={style.inputs}
-            name='end_time'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={input.end_time}
-            style={ showMsg.end_time && errors.end_time ? {border:'red 1px solid'}: {}}/>
-            {showMsg.end_time&&(
-                            <p className={style.warning}>{errors.end_time}</p>
-                        )}
+                type='time'
+                className={style.inputs}
+                name='end_time'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={input.end_time}
+                style={showMsg.end_time && errors.end_time ? { border: 'red 1px solid' } : {}} />
+            {showMsg.end_time && (
+                <p className={style.warning}>{errors.end_time}</p>
+            )}
+            </div>
         </div>
     )
 };
