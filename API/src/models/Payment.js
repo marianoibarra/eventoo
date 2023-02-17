c8801152bab8f72ff28882414a354bf7fa2a81b7
Ana Lucia Bulacios <analubula87@gmail.com>
2023-02-17 10:35:55 -0300

    modelmercadopago

--- API/src/models/Payment.js
+++ API/src/models/Payment.js
@@ -3,22 +3,28 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('payment',{
-        status: {
+        transactionId_MP: {
             type: DataTypes.STRING,
             allowNull: true,
         },          
-        payment_type:{
+        status: {
             type: DataTypes.STRING,
             allowNull: true,
         },             
-        total:{
+        price: {
             type: DataTypes.FLOAT,
             allowNull: true,
         }              
     })
 };
 
+//transactcion id para que el usuario pueda verificar en su mercado pago la compra. 
+ //: 
+ //status: 
+ //
 
+
+//payments.items[0].price 
 
 //un evento tiene un payment
 //un payment tiene un evento 
