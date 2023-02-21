6ecde8bdb534c468f8e9bb111d8dc883a992e839
NatuAls <nahuelalesso19@gmail.com>
2023-02-21 00:14:23 -0300

    create page NewReview.jsx

---
+++ Client/src/Pages/NewReview.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/NavBar/NavBar';
+import Review from '../Components/Review/Review';
+import Footer from '../Components/Footer/Footer';
+
+function NewReview() {
+  return (
+    <div>
+        <Navbar />
+        <Review />
+        <Footer />
+    </div>
+  )
+}
+
+export default NewReview;
