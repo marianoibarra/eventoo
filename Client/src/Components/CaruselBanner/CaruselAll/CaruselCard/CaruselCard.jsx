4503e5a85dd693a8214d79311843a70a7e98c19e
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-02-22 09:11:34 -0300

    commit

--- Client/src/Components/CaruselBanner/CaruselAll/CaruselCard/CaruselCard.jsx
+++ Client/src/Components/CaruselBanner/CaruselAll/CaruselCard/CaruselCard.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import Style from "./CaruselCard.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import moment from "moment";
-import { FaRegHeart, FaHeart } from 'react-icons/fa'
 import { HiOutlineHeart, HiHeart } from 'react-icons/hi'
 import { useDispatch, useSelector } from "react-redux";
 import { switchFavorites } from "../../../../Slice/Favorites/FavoritesSlice";
+import { SessionContext } from "../../../..";
 
 const CaruselCard = ({
   img,
@@ -26,14 +26,20 @@ const CaruselCard = ({
     return date;
   };
 
+  const {isLogged} = useSelector(state => state.user)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const date = start_time && start_date ? genDate() : null;
   const {favorites} = useSelector(state => state.favorites)
+  const { setShowSessionModal } = useContext(SessionContext)
 
   const handleFav = (e) => {
     e.stopPropagation()
-    dispatch(switchFavorites(id))
+    if(isLogged) {
+      setShowSessionModal('login')
+    } else {
+      dispatch(switchFavorites(id))
+    }
   }
 
   return (
@@ -52,14 +58,12 @@ const CaruselCard = ({
             }
           </div>
           {name && <h2 className={Style.details_title}>{name}</h2>}
-          {date && (
-            <span className={Style.details_date}>
-              {moment(date).add(1, "days").format("ddd, MMMM Do, h:mm")}
-            </span>
-          )}
-          <span className={Style.details_price}>
-            {isPaid ? `${price}` : "FREE"}
-          </span>
+          {date && <span className={Style.details_date} >{moment(date).format('ddd, MMMM Do, h:mm')}</span>}
+          <span className={Style.details_category}>{category}</span>
+          { isPaid 
+            ? <span className={Style.details_price}>{price}</span>
+            : <span className={Style.details_free}>FREE</span>
+          }
         </div>
       </div>
   );
