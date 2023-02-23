4f516cbe18c8a47f9fdea4229845a0cf18ea4e7c
fede-cid <fede.cid.96@gmail.com>
2023-02-23 01:27:12 -0300

    buys y create events for my events update

--- Client/src/Components/Tabs/Create/Create.jsx
+++ Client/src/Components/Tabs/Create/Create.jsx
@@ -1,30 +1,166 @@
-import React, { useEffect } from 'react'
-import {  useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
-import { axiosModeEventsCreateForUser } from '../../../Slice/EventsCreateForUser/CreateForUserSlice';
-import Styles from'./Create.module.css';
-function Create() {
-  const { events } = useSelector(state => state.eventsCreateForUserSlice)
-  const dispatch = useDispatch()
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { axiosModeEventsCreateForUser, setFiltercreateEvent, sortByAscendingEventscreate, sortByDescendingEventscreate } from '../../../Slice/EventsCreateForUser/CreateForUserSlice';
+import '../Buys/Events.css'
+import { FaEdit } from "react-icons/fa";
+import { TiArrowSortedDown, TiArrowUnsorted ,TiArrowSortedUp } from "react-icons/ti";
+import { AiOutlineCheck } from "react-icons/ai";
+import SearchBar from "../../Admin/SearchBar/SearchAdmin";
+
+function Buys() {
+  const { events, errorEvents } = useSelector((state) => state.eventsCreateForUserSlice);
+  const [sortType, setSortType] = useState({ type:null, id: 2 });
+  console.log(events);
+
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(axiosModeEventsCreateForUser());
   }, [dispatch]);
 
+  const handledelete = (e) => {
+    console.log(e);
+    // dispatch(deleteEvent(e));
+  };
+
+  const accent = (e) => {
+    if(sortType.type === e){
+    if (sortType.id === 2) {
+      setSortType({ type: e, id: 1 });
+      dispatch(sortByAscendingEventscreate(e));
+    }
+    if (sortType.id === 1) {
+      setSortType({ type: e, id: 2 });
+      dispatch(sortByDescendingEventscreate(e));
+    }}else{
+      setSortType({ type: e, id: 1 });
+      dispatch(sortByAscendingEventscreate(e));
+    }
+  };
+
+  const handleSearch = (key) => {
+    dispatch(setFiltercreateEvent(key));
+  };
+
   return (
-    <div className={Styles.container} >
-      {Array.isArray(events) && events.length !== 0 && events.map(event => (
-        <div className={Styles.eventCard}  key={event?.name}>
-          <Link to={`/Event/${event?.id}`}><h3 className={Styles.eventCardTitle}>Name: {event?.name}</h3></Link>
-          <p>Start date: {event?.start_date}</p>
-          <p>End date: {event?.end_date}</p>
-          <p>Category: {event?.category.name}</p>
-          <p>Age range: {event?.age_range}</p>
-    
-          <p className={event.isPublic ? Styles.public : Styles.private}>{event.isPublic ? 'Public' : 'Private'} </p>
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <h3>{errorEvents ? errorEvents : undefined}</h3>
+      <div className="sapList">
+        <div className="sapListHeader">
+          <div className="sapListItem">ID</div>
+          <div
+            className="sapListItem"
+            id={sortType.type == `organizer` ? "sapSelection" : undefined}
+          >
+            { sortType.type === 'organizer'? 
+            <TiArrowUnsorted
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("organizer")}
+            /> : <TiArrowSortedDown
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("organizer")}
+            /> }
+            Organizator
+          </div>
+          <div
+            className="sapListItem sapListItemWide"
+            id={sortType.type == `name` ? "sapSelection" : undefined}
+          >
+            { sortType.type === 'name'? 
+            <TiArrowUnsorted
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("name")}
+            /> : <TiArrowSortedDown
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("name")}
+            /> }
+            Name
+          </div>
+          <div
+            className="sapListItem sapListItemWide"
+            id={sortType.type == `start_date` ? "sapSelection" : undefined}
+          >
+            {sortType.type === 'start_date'? 
+            <TiArrowUnsorted
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("start_date")}
+            /> : <TiArrowSortedDown
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("start_date")}
+            /> }
+            Date
+          </div>
+          <div
+            className="sapListItem"
+            id={sortType.type == `isPremium` ? "sapSelection" : undefined}
+          >
+            { sortType.type === 'isPremium'? 
+            <TiArrowUnsorted
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("isPremium")}
+            /> : <TiArrowSortedDown
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("isPremium")}
+            /> }
+            Type
+          </div>
+          <div
+            className="sapListItem"
+            id={sortType.type == `status` ? "sapSelection" : undefined}
+          >
+            { sortType.type === 'status'? 
+            <TiArrowUnsorted
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("status")}
+            /> : <TiArrowSortedDown
+              size={18}
+              cursor="pointer"
+              onClick={() =>accent("status")}
+            /> }
+            Status
+          </div>
+          <div className="sapListItem">Action</div>
         </div>
-      ))}
+        {events.map((event,index) => (
+          <div className="sapListRow" key={index}>
+            <div className="sapListItem sap">{event.id}</div>
+            <div className="sapListItem sap">{`${event?.organizer?.name} ${event?.organizer?.last_name}`}</div>
+            <div className="sapListItem sapListItemWide sap">{event.name}</div>
+            <div className="sapListItem sapListItemWide sap">
+              {event?.start_date}
+            </div>
+            <div className="sapListItem sap">
+              {event?.isPremium ? "Premium" : "Free"}
+            </div>
+            <div className="sapListItem sap">
+              {event?.status}
+            </div>
+            <div className="sapListItem sap">
+              <button className="btnSap">
+                <FaEdit color="darkslateblue" size={25} />
+              </button>
+              <button className="btnSap">
+                <AiOutlineCheck
+                  onClick={() => handledelete(event.id)}
+                  size={35}
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Buys;
--- Client/src/Slice/EventsCreateForUser/CreateForUserSlice.js
+++ Client/src/Slice/EventsCreateForUser/CreateForUserSlice.js
@@ -17,7 +17,89 @@ export const eventsCreateForUserSlice = createSlice({
     loading: false,
     error: null
   },
-  reducers: {},
+  reducers: {
+    setFiltercreateEvent: (state, action) => {
+      const keyword = action.payload;
+      const results = state.eventsList.filter((event) =>
+        JSON.stringify(event).includes(keyword)
+      );
+      state.events = results.length ? results : state.events;
+      state.errorEvents = !results.length ? 'No data has been found' : undefined
+    },
+    sortByAscendingEventscreate: (state, action) => {
+      const propiedad = action.payload;
+      if (
+        propiedad === "start_date" ||
+        propiedad === "name" ||
+        propiedad === "status" 
+      ) {
+        state.events.sort((a, b) => {
+          if (
+            !a[propiedad] ||
+            !b[propiedad] ||
+            typeof a[propiedad] !== "string" ||
+            typeof b[propiedad] !== "string"
+          ) {
+            return 0; 
+          }
+          return a[propiedad].localeCompare(b[propiedad]);
+        });
+      } else if (propiedad === "organizer") {
+        state.events.sort((a, b) => {
+          let c = a[propiedad] && a[propiedad].name;
+          let d = b[propiedad] && b[propiedad].name;
+          if (
+            !c ||
+            !d ||
+            typeof c !== "string" ||
+            typeof d !== "string"
+          ) {
+            return 0;
+          }
+          return c.localeCompare(d);
+        });
+      }else {
+        state.events.sort((a, b) => a[propiedad] - b[propiedad]);
+      }
+    },
+    sortByDescendingEventscreate: (state, action) => {
+      const propiedad = action.payload;
+      if (
+        propiedad === "start_date" ||
+        propiedad === "name"  ||
+        propiedad === "status"
+      ) {
+        state.filteredDataUser = state.events.sort((a, b) => {
+          if (
+            !a[propiedad] ||
+            !b[propiedad] ||
+            typeof a[propiedad] !== "string" ||
+            typeof b[propiedad] !== "string"
+          ) {
+            return 0;
+          }
+          return b[propiedad].localeCompare(a[propiedad]);
+        });
+      } else if (propiedad === "organizer") {
+        state.events.sort((a, b) => {
+          let c = a[propiedad] && a[propiedad].name;
+          let d = b[propiedad] && b[propiedad].name;
+          if (
+            !c ||
+            !d ||
+            typeof c !== "string" ||
+            typeof d !== "string"
+          ) {
+            return 0;
+          }
+          return d.localeCompare(c);
+        });
+      
+      }else {
+        state.events.sort((a, b) => b[propiedad] - a[propiedad]);
+      }
+    },
+  },
   extraReducers: {
     [axiosModeEventsCreateForUser.pending]: (state) => {
       state.loading = true
@@ -35,4 +117,12 @@ export const eventsCreateForUserSlice = createSlice({
   }
 }
 )
+
+export const {
+  setFiltercreateEvent,
+  sortByAscendingEventscreate,
+  sortByDescendingEventscreate
+  } = eventsCreateForUserSlice
+  .actions;
+
 export default eventsCreateForUserSlice.reducer
