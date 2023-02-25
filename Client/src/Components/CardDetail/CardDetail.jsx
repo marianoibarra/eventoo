a613ae4db7435925b6d698881afcb008fd161f54
NatuAls <nahuelalesso19@gmail.com>
2023-02-24 21:08:16 -0300

    refactor CardDetail.jsx and css

--- Client/src/Components/CardDetail/CardDetail.jsx
+++ Client/src/Components/CardDetail/CardDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
   axiosModeEventDetail,
@@ -7,19 +7,23 @@ import {
   axiosGetEventPrivate,
   clear,
 } from "../../Slice/EventDetail/EventDetailSlice";
+import { clearEventsManagement, getEventsManagement } from "../../Slice/eventsManagement/eventsManagementSlice";
 import EventInformation from "./EventInformation/EventInformation";
 import EventLocation from "./EventLocation/EventLocation";
 import BuyButton from "./BuyButton/BuyButton";
 import covers from "../../imgs/covers";
+import moment from "moment";
 import { AiTwotoneCalendar, AiFillEdit } from "react-icons/ai";
 import { RiTicket2Fill } from "react-icons/ri";
 import style from "./CardDetail.module.css";
-import { Link } from "react-router-dom";
+
+const lorem = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
 const CardDetailPublic = () => {
   const { eventDetail, loading, error } = useSelector(
     (state) => state.eventDetail
   );
+  const { data } = useSelector(state => state.eventsManagement);
   const dispatch = useDispatch();
   const { id } = useParams();
 
@@ -36,6 +40,25 @@ const CardDetailPublic = () => {
     edited: false,
   });
   const [errors, setErrors] = useState({});
+
+  const genDate = () => {
+    const [hour, minute] = eventDetail.start_time.split(":");
+    const date = new Date(eventDetail.start_date);
+    date.setHours(hour);
+    date.setMinutes(minute);
+    return date;
+  };
+
+  const genEndDate = () => {
+    const [hour, minute] = eventDetail.end_time.split(":");
+    const date = new Date();
+    date.setHours(hour);
+    date.setMinutes(minute);
+    return date.getTime();
+  }
+
+  const date = eventDetail.start_date && eventDetail.start_time ? genDate() : null;
+  const endDate = eventDetail.end_time ? genEndDate() : null;
 
   //funciones de usuario organizador
   function validate(input) {
@@ -82,114 +105,114 @@ const CardDetailPublic = () => {
   }
 
   useEffect(() => {
-    if (eventDetail.organizer) {
-      if (user.id !== null && user.id === eventDetail.organizer.id) {
-        setOrganizer(true);
-        setEditedEvent({
-          ...editedEvent,
-          name: eventDetail.name,
-          description: eventDetail.description,
-        });
+    dispatch(getEventsManagement());
+    if (user.isLogged) {
+      if (eventDetail.organizer) {
+        if (user.id === eventDetail.organizer.id) {
+          setOrganizer(true);
+          setEditedEvent({
+            ...editedEvent,
+            name: eventDetail.name,
+            description: eventDetail.description,
+          });
+        }
       }
+    }
+    return() => {
+      setOrganizer(false);
+      dispatch(clear());
     }
   }, [eventDetail, user]);
 
   return (
     <>
-      {eventDetail && Object.keys(eventDetail).length > 0 && (
+      {eventDetail && data && Object.keys(eventDetail).length > 0 && (
         <>
-          <div className={style.containertop}>
-            <div className={style.container_img_and_h1}>
-              <div className={style.containerimg}>
-                <img
-                  src={
-                    eventDetail.cover_pic
-                      ? eventDetail.cover_pic
-                      : covers[eventDetail.category.name]
-                  }
-                  alt="cover_pic"
-                />
+          <div className={style.body}>
+            <div className={style.containerimg}>
+              <img
+                src={
+                  eventDetail.cover_pic
+                    ? eventDetail.cover_pic
+                    : covers[eventDetail.category.name]
+                }
+                alt="cover_pic"
+              />
+            </div>
+            <div className={style.container_title_and_description}>
+              <div className={style.container_title}>
+                {edit.name === false ? (
+                  <h1>
+                    {eventDetail.name && eventDetail.name}{" "}
+                  </h1>
+                ) : (
+                  <div className={style.organizerdiv}>
+                    <input
+                      className={
+                        errors.name
+                          ? style.organizerinput_error
+                          : style.organizerinput
+                      }
+                      type="text"
+                      name="name"
+                      value={editedEvent.name}
+                      onChange={handleOnChange}
+                    />
+                    {organizer === true && (
+                      <a className={style.organizericon} onClick={editButton}>
+                        <AiFillEdit size={30} />
+                      </a>
+                    )}
+                    {organizer === true && errors.name && <p>{errors.name}</p>}
+                  </div>
+                )}
               </div>
-              {edit.name === false ? (
-                <h1>
-                  {eventDetail.name?.toUpperCase()}{" "}
-                  {organizer === true && (
-                    <a id="name" onClick={editButton}>
-                      <AiFillEdit size={35} />
-                    </a>
-                  )}
-                </h1>
-              ) : (
-                <div className={style.organizerdiv}>
-                  <input
+              <div className={style.containerdescription}>
+                {edit.description === false ? (
+                  <p>{eventDetail.description}</p>
+                ) : (
+                  <textarea
                     className={
-                      errors.name
-                        ? style.organizerinput_error
-                        : style.organizerinput
+                      errors.description
+                        ? style.textarea_error
+                        : style.textarea_border
                     }
-                    type="text"
-                    name="name"
-                    value={editedEvent.name}
+                    name="description"
+                    value={editedEvent.description}
                     onChange={handleOnChange}
                   />
-                  {organizer === true && (
-                    <a className={style.organizericon} onClick={editButton}>
-                      <AiFillEdit size={35} />
-                    </a>
-                  )}
-                  {organizer === true && errors.name && <p>{errors.name}</p>}
-                </div>
-              )}
-            </div>
-            <div className={style.containerdescription}>
-              {edit.description === false ? (
-                <p>{eventDetail.description}</p>
-              ) : (
-                <textarea
-                  className={
-                    errors.description
-                      ? style.textarea_error
-                      : style.textarea_border
-                  }
-                  name="description"
-                  value={editedEvent.description}
-                  onChange={handleOnChange}
-                />
-              )}
-              {organizer === true && (
-                <a
-                  id="description"
-                  className={style.organizericon}
-                  onClick={editButton}
-                >
-                  <AiFillEdit size={35} />
-                </a>
-              )}
-            </div>
-            {organizer === true && errors.description && (
+                )}
+                
+                {/* {organizer === true && (
+                  <a
+                    id="description"
+                    className={style.organizericon}
+                    onClick={editButton}
+                  >
+                    <AiFillEdit size={30} />
+                  </a>
+                )} */}
+                <hr></hr>
+              </div>
+
+              {organizer === true && errors.description && (
               <p className={style.textdescription_error}>
                 {errors.description}
               </p>
-            )}
-          </div>
+              )}
 
-          <EventInformation />
+            </div>
 
-          <div className={style.containerbottom}>
-            <div className={style.containerdate}>
+            <div className={style.container_date}>
               <div className={style.containericon}>
                 <span className={style.iconspan}>
                   {" "}
-                  <AiTwotoneCalendar size={35} />{" "}
+                  <AiTwotoneCalendar size={30} />{" "}
                 </span>
                 <span className={style.iconspantext}>Date and Time</span>
               </div>
               <h3>
-                {eventDetail.start_date &&
-                  `${eventDetail.start_date.replace(
-                    /^(\d{4})-(\d{2})-(\d{2})$/g,
-                    "$3/$2/$1"
-                  )}, ${eventDetail.start_time}hs`}
+                {`${date && moment(date).format('ddd, MMMM Do, h:mm')} to ${endDate && moment(endDate).format('h:mm')}`}
               </h3>
             </div>
 
@@ -197,55 +220,27 @@ const CardDetailPublic = () => {
               <EventLocation />
             )}
 
-            <div className={style.containerdate}>
+            <div className={style.container_date}>
               <div className={style.containericon}>
                 <span className={style.iconspan}>
                   {" "}
                   <RiTicket2Fill size={35} />{" "}
                 </span>
                 <span className={style.iconspantext}>About the event</span>
-              </div>
+              </div> 
               <div className={style.aboutevent}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <img
-                  src="https://us.cdn.eltribuno.com/042016/1487154515198.jpeg"
-                  alt="party"
-                />
+                <p>{eventDetail.large_description ? eventDetail.large_description : lorem}</p>
               </div>
+              <hr></hr>
             </div>
 
+            <EventInformation />
+
             {organizer === false && (
-              <BuyButton organizer={organizer} edited={editedEvent.edited} />
+              <BuyButton />
             )}
 
-            {organizer === true && (
+            {user.isLogged && organizer === true && (
               <div className={style.container_organizerbutton}>
                 <div className={style.organizerbutton_div}>
                   <div className={style.organizerbutton}>
