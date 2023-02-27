ce3b710c8fedeb653c27c119cc3d2122f23b44bb
marylaurab <mbetancourt@fi.uba.ar>
2023-02-27 02:19:45 -0300

    Creacion de nuevo modelo (Notifications),limpieza del modelo user (quitando lo de commits anteriores q no funcionaron) y relaciones del nuevo modelo listas

--- API/src/models/User.js
+++ API/src/models/User.js
@@ -54,10 +54,6 @@ module.exports = (sequelize) => {
         emailIsVerify: {
           type: DataTypes.BOOLEAN,
           defaultValue: false
-        },  
-        notified:{
-          type: DataTypes.BOOLEAN,
-          defaultValue: false
-        }   
+        }
     })
 };
