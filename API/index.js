64d9948badbe7fb9afe0da7791ae97e5da7e5940
Mary Laura Betancourt <mbetancourt@fi.uba.ar>
2023-02-28 14:25:23 -0300

    cron inicializado

--- API/index.js
+++ API/index.js
@@ -3,8 +3,8 @@ const { conn } = require("./src/db.js");
 require("dotenv").config();
 const fillTables = require("./src/helpers/fillTables.js");
 const { createSuperAdmin } = require("./src/helpers/createSuperAdmin.js");
-const data=require('./src/mocks/superAdmin.js')
-
+const data = require("./src/mocks/superAdmin.js");
+const cronJob = require("./src/helpers/finishedEvents.js");
 
 const port = process.env.PORT || 3001;
 
@@ -13,10 +13,10 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-conn.sync({ alter: true })
+conn
+  .sync({ alter: true })
   .then(() => fillTables())
-  .then(()=> createSuperAdmin(data))
-  .then(() =>
-    app.listen(port, () => console.log(`Listening on port ${port}!`))
-  )
+  .then(() => createSuperAdmin(data))
+  .then(() => cronJob.start())
+  .then(() => app.listen(port, () => console.log(`Listening on port ${port}!`)))
   .catch((e) => console.log(e));
