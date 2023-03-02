48ba37311d26893e018b2b285bf47cc97c47f2fb
NatuAls <nahuelalesso19@gmail.com>
2023-03-01 21:25:21 -0300

    refactor components

--- Client/src/Components/CardDetail/CardDetail.jsx
+++ Client/src/Components/CardDetail/CardDetail.jsx
@@ -25,8 +25,6 @@ import { SessionContext } from "../..";
 import { HiOutlineHeart, HiHeart } from 'react-icons/hi'
 import { switchFavorites } from "../../Slice/Favorites/FavoritesSlice";
 
-const lorem = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
-
 const sx = {
   bgcolor: "#BC4001",
   color: "white",
@@ -266,9 +264,10 @@ const CardDetailPublic = () => {
                 </span>
                 <span className={style.iconspantext}>About the event</span>
               </div>
-              <div className={style.aboutevent} dangerouslySetInnerHTML={{ __html: eventDetail.large_description 
-                ? eventDetail.large_description.replace(/\n/g, '<br>') 
-                : lorem }}></div>
+              <div className={style.aboutevent} dangerouslySetInnerHTML={{ __html: eventDetail.large_description
+                ? eventDetail.large_description.replace(/\n/g, '<br>')
+                : eventDetail.description
+              }}></div>
               <hr></hr>
             </div>
 
--- Client/src/Components/CardDetail/ContainerButtonRight/ContainerButtonRight.jsx
+++ Client/src/Components/CardDetail/ContainerButtonRight/ContainerButtonRight.jsx
@@ -1,33 +1,18 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { axiosModeEditEventDetail, axiosModeEventDetail, setEditedEvent, setErrors, setModeEdit } from "../../../Slice/EventDetail/EventDetailSlice";
-import { HiOutlineHeart, HiHeart } from 'react-icons/hi'
-import { AiFillEdit } from "react-icons/ai";
+import { axiosModeEditEventDetail, setEditedEvent, setErrors, setModeEdit } from "../../../Slice/EventDetail/EventDetailSlice";
 import BuyButton from "./BuyButton/BuyButton";
 import UserIcon from "../../../Assets/UserProfile.png";
 import style from "./ContainerButtonRight.module.css";
 import ModalReviews from "../../Modal/ModalReviews/ModalReviews";
 import { Rating } from "@mui/material";
-import { Chip } from "@mui/material";
-import { SessionContext } from "../../..";
-import { switchFavorites } from "../../../Slice/Favorites/FavoritesSlice";
-
-const sx = {
-  mt: "10px",
-  bgcolor: "#BC4001",
-  color: "white"
-}
 
 function ContainerButtonRight() {
 
   const user = useSelector((state) => state.user);
-  const {isLogged} = useSelector(state => state.user);
   const { eventDetail } = useSelector(state => state.eventDetail);
   const { organizer, editedEvent, errors} = useSelector(state => state.eventDetail.eventEdition);
-  const {favorites, loading} = useSelector(state => state.favorites);
-  const { setShowSessionModal } = useContext(SessionContext);
-  const [thisLoading, setThisLoading] = useState(false);
   const { id } = useParams();
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
@@ -48,49 +33,9 @@ function ContainerButtonRight() {
     }
   }
 
-  const handleFav = (e) => {
-    e.stopPropagation()
-    if(isLogged) {
-      setThisLoading(true)
-      dispatch(switchFavorites(id))
-    } else {
-      setShowSessionModal('login')
-    }
-  }
-
-  function editButton(event) {
-    event.preventDefault();
-    dispatch(setModeEdit(true));
-  }
-
   return (
     <div className={style.background}>
       {showModal && <ModalReviews setShowModal={setShowModal} />}
-      
-      {/* <div className={style.category_and_age}>
-        {eventDetail.category && 
-          <Chip sx={sx} label={eventDetail.category.name}  />              
-        }
-
-        <Chip label={eventDetail.age_range} sx={sx} />
-
-        {!organizer && 
-          <div className={style.favorite} onClick={handleFav}>
-            {
-              favorites.some(f => f === id)
-              ? <HiHeart className={style.favEnabled} size={20} />
-              : <HiOutlineHeart size={20} />
-            }
-            {loading && thisLoading && <div class={style.loadingRing}></div>}
-          </div>
-        }
-
-        {organizer === true &&
-          <a className={style.organizericon} onClick={editButton}>
-            <AiFillEdit size={30} />
-          </a>
-        }
-      </div> */}
 
       {eventDetail.organizer.name && <div className={style.container_organizer}  onClick={() => {setShowModal(true)}}>
 
