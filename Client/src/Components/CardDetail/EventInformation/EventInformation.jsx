7642f9aa36e80f875cf296f20a18cc0fe61e550f
NatuAls <nahuelalesso19@gmail.com>
2023-03-02 23:27:20 -0300

    refactor EventInfromation and styles

--- Client/src/Components/CardDetail/EventInformation/EventInformation.jsx
+++ Client/src/Components/CardDetail/EventInformation/EventInformation.jsx
@@ -16,39 +16,39 @@ const EventInformation = () => {
 
                 {eventDetail.disability_access === true &&
                     <div className={style.containericon}> 
-                        <span className={style.iconspan}> <TbDisabled size={30}/> </span>
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <TbDisabled size={30}/> </span>
                         <span className={style.iconspantext}>Disability Access</span> 
                     </div>
                 }
 
                 {eventDetail.parking === true &&
                     <div className={style.containericon}>   
-                        <span className={style.iconspan}> <TbParking size={30}/> </span>
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <TbParking size={30}/> </span>
                         <span className={style.iconspantext}>Parking</span>
                     </div>
                 }
 
                 {eventDetail.pet_friendly === true &&
                     <div className={style.containericon}> 
-                        <span className={style.iconspan}> <MdPets size={30}/> </span> 
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <MdPets size={30}/> </span> 
                         <span className={style.iconspantext}>Pet Friendly</span> 
                     </div>                 
                 }
 
                 {eventDetail.smoking_zone === true &&
                     <div className={style.containericon}> 
-                        <span className={style.iconspan}> <MdSmokingRooms size={30}/> </span> 
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <MdSmokingRooms size={30}/> </span> 
                         <span className={style.iconspantext}>Smoking Zone</span> 
                     </div>             
                 }
 
                 {eventDetail.isPublic === true ?
                     <div className={style.containericon}> 
-                        <span className={style.iconspan}> <BsFillPeopleFill size={30}/> </span> 
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <BsFillPeopleFill size={30}/> </span> 
                         <span className={style.iconspantext}>Public Access</span> 
                     </div> :
                     <div className={style.containericon}> 
-                        <span className={style.iconspan}> <RiGitRepositoryPrivateFill size={30}/> </span> 
+                        <span className={eventDetail.typePack === 'PREMIUM' ? style.iconspan_premium : style.iconspan}> <RiGitRepositoryPrivateFill size={30}/> </span> 
                         <span className={style.iconspantext}>Private Access</span> 
                     </div>              
                 }
